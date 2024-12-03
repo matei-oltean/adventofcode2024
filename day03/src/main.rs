c2bde75c73ffd7860a15173213b1c49ed24dd077
matei-oltean <matei.oltean@gmail.com>
2024-12-03 13:11:21 +0100

    Add day03 02

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,13 +2,27 @@ use std::fs;
 
 use regex::Regex;
 
-fn main() {
+fn sum_mult(input: &str) -> i32 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let input = fs::read_to_string("./input/03").unwrap();
-    let res = re
-        .captures_iter(&input)
+    re.captures_iter(input)
         .map(|caps| caps.extract())
         .map(|(_, [x, y])| x.parse::<i32>().unwrap() * y.parse::<i32>().unwrap())
+        .sum::<i32>()
+}
+
+fn main() {
+    let input = fs::read_to_string("./input/03")
+        .unwrap()
+        .lines()
+        .collect::<String>();
+
+    println!("Result: {}", sum_mult(input.as_str()));
+
+    let re = Regex::new(r"(?:^|do\(\))(.*?)(?:don't\(\)|$)").unwrap();
+    let result = re
+        .captures_iter(&input)
+        .map(|caps| caps.extract())
+        .map(|(_, [s])| sum_mult(s))
         .sum::<i32>();
-    println!("Result: {}", res);
+    println!("Result: {}", result);
 }
